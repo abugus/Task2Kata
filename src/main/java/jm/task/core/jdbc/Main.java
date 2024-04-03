08177aeb150ed3ef33c463a8475139c88c20b48a
Abubakar Gusaev <abakar.gusaev@gmail.com>
2024-04-03 22:46:32 +0300

    сÐchange

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,34 +2,36 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
         System.out.println("Таблица создана");
 
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name1", "LastName1", (byte) 20);
         System.out.println("User с именем — Name1 добавлен в базу данных");
 
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
         System.out.println("User с именем — Name2 добавлен в базу данных");
 
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
         System.out.println("User с именем — Name3 добавлен в базу данных");
 
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
         System.out.println("User с именем — Name4 добавлен в базу данных");
 
-        System.out.println(userDao.getAllUsers());
+        System.out.println(userService.getAllUsers());
 
-        userDao.cleanUsersTable();
+        userService.cleanUsersTable();
         System.out.println("Очистка таблицы User(ов) прошла успешно");
 
-        userDao.cleanUsersTable();
-        System.out.println(" Удаление таблицы users прошло успешно");
+        userService.cleanUsersTable();
+        System.out.println("Удаление таблицы users прошло успешно");
 
         if(Util.getConnection()!=null) {
             Util.closeConnection();
